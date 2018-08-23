021fdf1a29abc252c410081086a6c37673f34839
Monica  Karasek <mlkarasek@gmail.com>
2018-08-23 19:50:19 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,20 +1,16 @@
 class GenresController < ApplicationController
-
-  def index
-    @genres = genre.all
-  end
-
-  def show
-    @genre = Genre.find(params[:id])
-  end
-
   def new
     @genre = Genre.new
   end
 
   def create
-    @genre = Genre.create(genre_params(:name))
+    @genre = Genre.new(post_params(:name))
+    @genre.save
     redirect_to genre_path(@genre)
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
   end
 
   def edit
@@ -26,4 +22,10 @@ class GenresController < ApplicationController
     @genre.update(post_params(:name))
     redirect_to genre_path(@genre)
   end
+
+  private
+
+	def post_params(*args)
+	  params.require(:genre).permit(*args)
+	end
 end
